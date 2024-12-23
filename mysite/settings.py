e38b74716dbd0830a8b9d1590002fa4b3be1d22c
SUINJU <jusuin5876@gmail.com>
2024-12-23 19:47:38 +0900

    blog Update and Security and Comment add features.

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,3 +126,5 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,re_path
+
+from django.contrib.auth import views
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('blog/', include('blog.urls')),
-    
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^accounts/login/$",views.LoginView.as_view(),name='login'),
+    re_path(r"^accounts/logout/$",views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
+    path(r'', include('blog.urls')),
 ]
